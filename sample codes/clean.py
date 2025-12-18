255613915491387615eef993af2f07c1b0a3abcb
Ushaoo <yshenbm@connect.ust.hk>
2025-12-18 17:14:03 +0800

    delete useless files

--- sample codes/clean.py
+++
@@ -1,9 +0,0 @@
-import Hobot.GPIO as GPIO
-
-try:
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.cleanup()
-    print("GPIO is cleanned successfully.")
-except Exception as e:
-    print(f"Error Message: {e}")
-    exit()
